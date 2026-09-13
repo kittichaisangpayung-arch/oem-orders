9f6cf52f4c5bf223e174fdf28aba32ce6856e6c1
Ter <Kittichaisangpayung@gmail.com>
2026-09-13 14:41:27 +0700

    Simplify GoogleDriveStorage backend - remove tempfile dependency

--- oem_orders/storage_backends.py
+++ oem_orders/storage_backends.py
@@ -11,9 +11,7 @@ import json
 
 
 class GoogleDriveStorage(Storage):
-    """
-    Custom storage class for Google Drive
-    """
+    """Custom storage class for Google Drive"""
 
     def __init__(self):
         self._drive = None
@@ -24,33 +22,11 @@ class GoogleDriveStorage(Storage):
         if self._drive is None:
             gauth = GoogleAuth()
 
-            # Try to load credentials from environment variable
-            credentials_json = os.environ.get('GDRIVE_CREDENTIALS_JSON', '')
-
-            if credentials_json:
-                # Create temporary settings file
-                settings = {
-                    "client_config_backend": "settings",
-                    "client_config": json.loads(credentials_json),
-                    "save_credentials": True,
-                    "save_credentials_backend": "file",
-                    "save_credentials_file": "credentials.json",
-                    "get_refresh_token": True,
-                    "oauth_scope": ["https://www.googleapis.com/auth/drive"]
-                }
-
-                with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.yaml') as f:
-                    import yaml
-                    yaml.dump(settings, f)
-                    settings_file = f.name
-
-                gauth.settings_file = settings_file
-
-            # Load credentials or authenticate
+            # Load credentials
             gauth.LoadCredentialsFile("credentials.json")
 
             if gauth.credentials is None:
-                gauth.LocalWebserverAuth()
+                raise Exception("No credentials found")
             elif gauth.access_token_expired:
                 gauth.Refresh()
             else:
@@ -60,18 +36,6 @@ class GoogleDriveStorage(Storage):
             self._drive = GoogleDrive(gauth)
 
         return self._drive
-
-    def _open(self, name, mode='rb'):
-        """Open a file from Google Drive"""
-        drive = self._get_drive()
-        file_id = self._get_file_id(name)
-
-        if file_id:
-            gdrive_file = drive.CreateFile({'id': file_id})
-            content = gdrive_file.GetContentString()
-            return File(io.BytesIO(content.encode()), name=name)
-
-        raise FileNotFoundError(f"File {name} not found in Google Drive")
 
     def _save(self, name, content):
         """Save a file to Google Drive"""
@@ -97,7 +61,7 @@ class GoogleDriveStorage(Storage):
 
         gdrive_file.Upload()
 
-        # Store file mapping (file path -> file ID)
+        # Store file mapping
         self._save_file_mapping(name, gdrive_file['id'])
 
         return name
@@ -109,14 +73,12 @@ class GoogleDriveStorage(Storage):
         current_parent = parent_id
 
         for folder_name in folders:
-            # Check if folder exists
             query = f"title='{folder_name}' and '{current_parent}' in parents and mimeType='application/vnd.google-apps.folder' and trashed=false"
             file_list = drive.ListFile({'q': query}).GetList()
 
             if file_list:
                 current_parent = file_list[0]['id']
             else:
-                # Create folder
                 folder = drive.CreateFile({
                     'title': folder_name,
                     'parents': [{'id': current_parent}],
@@ -129,7 +91,8 @@ class GoogleDriveStorage(Storage):
 
     def _save_file_mapping(self, path, file_id):
         """Save mapping of file path to Google Drive file ID"""
-        mapping_file = 'gdrive_mapping.json'
+        from django.conf import settings
+        mapping_file = os.path.join(settings.BASE_DIR, 'gdrive_mapping.json')
 
         if os.path.exists(mapping_file):
             with open(mapping_file, 'r') as f:
@@ -144,7 +107,8 @@ class GoogleDriveStorage(Storage):
 
     def _get_file_id(self, name):
         """Get Google Drive file ID from path"""
-        mapping_file = 'gdrive_mapping.json'
+        from django.conf import settings
+        mapping_file = os.path.join(settings.BASE_DIR, 'gdrive_mapping.json')
 
         if os.path.exists(mapping_file):
             with open(mapping_file, 'r') as f:
@@ -153,8 +117,19 @@ class GoogleDriveStorage(Storage):
 
         return None
 
+    def exists(self, name):
+        """Check if a file exists"""
+        return self._get_file_id(name) is not None
+
+    def url(self, name):
+        """Return the URL to access a file"""
+        file_id = self._get_file_id(name)
+        if file_id:
+            return f"{self.base_url}{file_id}"
+        return ""
+
     def delete(self, name):
-        """Delete a file from Google Drive"""
+        """Delete a file"""
         drive = self._get_drive()
         file_id = self._get_file_id(name)
 
@@ -162,37 +137,8 @@ class GoogleDriveStorage(Storage):
             gdrive_file = drive.CreateFile({'id': file_id})
             gdrive_file.Trash()
 
-    def exists(self, name):
-        """Check if a file exists in Google Drive"""
-        return self._get_file_id(name) is not None
-
-    def listdir(self, path):
-        """List contents of a directory"""
-        drive = self._get_drive()
-        folder_id = os.environ.get('GDRIVE_FOLDER_ID', 'root')
-
-        if path:
-            folder_id = self._get_folder_id(path, folder_id)
-
-        if not folder_id:
-            return [], []
-
-        query = f"'{folder_id}' in parents and trashed=false"
-        file_list = drive.ListFile({'q': query}).GetList()
-
-        directories = []
-        files = []
-
-        for item in file_list:
-            if item['mimeType'] == 'application/vnd.google-apps.folder':
-                directories.append(item['title'])
-            else:
-                files.append(item['title'])
-
-        return directories, files
-
     def size(self, name):
-        """Return the size of a file"""
+        """Return file size"""
         drive = self._get_drive()
         file_id = self._get_file_id(name)
 
@@ -201,57 +147,3 @@ class GoogleDriveStorage(Storage):
             return int(gdrive_file.get('fileSize', 0))
 
         return 0
-
-    def url(self, name):
-        """Return the URL to access a file"""
-        file_id = self._get_file_id(name)
-
-        if file_id:
-            return f"{self.base_url}{file_id}"
-
-        return ""
-
-    def _get_folder_id(self, path, parent_id='root'):
-        """Get folder ID from path"""
-        drive = self._get_drive()
-        folders = path.split('/')
-        current_parent = parent_id
-
-        for folder_name in folders:
-            query = f"title='{folder_name}' and '{current_parent}' in parents and mimeType='application/vnd.google-apps.folder' and trashed=false"
-            file_list = drive.ListFile({'q': query}).GetList()
-
-            if file_list:
-                current_parent = file_list[0]['id']
-            else:
-                return None
-
-        return current_parent
-
-    def get_accessed_time(self, name):
-        """Return the last accessed time"""
-        return self.get_modified_time(name)
-
-    def get_created_time(self, name):
-        """Return the creation time"""
-        drive = self._get_drive()
-        file_id = self._get_file_id(name)
-
-        if file_id:
-            gdrive_file = drive.CreateFile({'id': file_id})
-            from datetime import datetime
-            return datetime.strptime(gdrive_file['createdDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
-
-        return None
-
-    def get_modified_time(self, name):
-        """Return the last modified time"""
-        drive = self._get_drive()
-        file_id = self._get_file_id(name)
-
-        if file_id:
-            gdrive_file = drive.CreateFile({'id': file_id})
-            from datetime import datetime
-            return datetime.strptime(gdrive_file['modifiedDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
-
-        return None
